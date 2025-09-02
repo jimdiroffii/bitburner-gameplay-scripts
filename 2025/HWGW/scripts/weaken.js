9b5af2d4882538726659a27ab0973d879a62595a
Jim Diroff II <jim@dirofftech.com>
2025-09-02 18:37:16 -0400

    HWGW updates

--- 2025/HWGW/scripts/weaken.js
+++ 2025/HWGW/scripts/weaken.js
@@ -1,4 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    await ns.weaken(ns.args[0]);
+    // args[0]: target server (string)
+    // args[1]: delay in milliseconds (number)
+    const target = ns.args[0];
+    const delay = ns.args[1];
+
+    if (delay > 0) {
+        await ns.sleep(delay);
+    }
+
+    await ns.weaken(target);
 }
